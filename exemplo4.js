6e6fc575d6cf477b375dd3a5b4e9a5e02f71be06
willian rafael <willianrafael050403@gmail.com>
2024-09-16 09:40:18 -0300

    inicio.js

---
+++ exemplo4.js
@@ -0,0 +1,8 @@
+function mostra( ){
+    var idade = document.form1.txtIdade.value ; //usando o name
+    var idade1 = document.getElementById("txtIdade").value; // usando id
+    alert(idade);
+    alert(idade1);
+}
+
+
